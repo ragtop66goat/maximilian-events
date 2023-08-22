701a0d160cbce989d2945c766c84846db3f2bc25
ragtop66goat <ragtop66goat@gmail.com>
2023-08-22 09:39:33 -0500

    added getStaticProps calls to [eventid].js and pages/index.js to retrieve data from realtime database

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,27 +1,58 @@
-import {useRouter} from "next/router";
-import {getEventById} from "@/dummy-data";
 import EventSummary from "@/components/event-detail/event-summary";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventContent from "@/components/event-detail/event-content";
-import {Fragment} from "react";
 
-function EventDetailPage() {
+function EventDetailPage(props) {
 
-    const router = useRouter();
-    const eventId = router.query.eventid;
-    const event = getEventById(eventId);
+  const event = props.selectedEvent[0];
 
-    if(!event) return <h1>No Event Found </h1>
+  if (!event) return <h1>No Event Found </h1>
 
-    return (
-      <Fragment>
-          <EventSummary title={event.title}/>
-        <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
-        <EventContent>
-          <p>{event.description}</p>
-        </EventContent>
-      </Fragment>
-    )
+  return (
+    <>
+      <EventSummary title={event.title}/>
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  )
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventid;
+  return fetch(`https://nextjs-course-cea5e-default-rtdb.firebaseio.com/events.json?orderBy="id"&equalTo="${eventId}"`)
+    .then(response => response.json())
+    .then(data => {
+      const selectedEvent = [];
+      for (const key in data) {
+        selectedEvent.push({
+          id: key,
+          description: data[key].description,
+          image: data[key].image,
+          isFeatured: data[key].isFeatured,
+          title: data[key].title,
+          location: data[key].location
+        })
+      }
+      console.log("Selected Event " + selectedEvent);
+      return {props: {selectedEvent: selectedEvent}};
+    })
+}
+
+export async function getStaticPaths() {
+  return fetch('https://nextjs-course-cea5e-default-rtdb.firebaseio.com/events.json')
+    .then(response => response.json())
+    .then(data => {
+      const ids = [];
+      for(const key in data) {
+        ids.push({params: {eventid: key}})
+      }
+      return {
+        paths: ids,
+        fallback: true
+      }
+    })
 }
 
 export default EventDetailPage;
